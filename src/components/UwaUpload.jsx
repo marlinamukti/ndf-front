ef1aba4d90bc635447e6b5e4a503c1e3dd1693f5
andita.kaesar <andita.kaesar@xtremax.com>
2019-06-27 18:24:06 +0700

    implement upload image

--- src/components/UwaUpload.jsx
+++ src/components/UwaUpload.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Image, Transformation, CloudinaryContext } from 'cloudinary-react';
-import { NONAME } from 'dns';
+import { Image } from 'cloudinary-react';
+import Axios from 'axios';
 
 const hideDisplay = {
     display: 'none'
@@ -12,7 +12,8 @@ class UwaUpload extends React.Component {
         this.state = {
             files: [],
             cloudName: "ndf001dev",
-            publicId: "samples/bike.jpg"
+            publicId: "samples/bike.jpg",
+            showBeginUpload: false
         }
     }
 
@@ -28,12 +29,48 @@ class UwaUpload extends React.Component {
         let newFile = [];
         newFile.push(target.files[0]);
         this.setState({
-            files: newFile
+            files: newFile,
+            showBeginUpload: true
         });
+    }
+
+    acceptedFile() {
+        // reserved for future use
+        return "image/*";
     }
 
     handleClickUpload = (event) => {
         this.refs.fileUploader.click();
+    }
+
+    handleBeginUpload = (event) => {
+        this.uploadFile();
+    }
+
+    uploadFile() {
+        let data = new FormData();
+        let file = this.state.files[0];
+        let url = 'http://localhost:4000/testupload';
+        data.append('file', file);
+        data.append('name', file.name);
+
+        Axios.post(url, data).then(res => {
+            console.log(res);
+        }).catch(error => {
+            console.log(error);
+        })
+    }
+
+    renderDisplay() {
+        if (this.state.showBeginUpload) {
+            return {
+                display: 'inline'
+            }
+        } else {
+            return {
+                display: 'none'
+            }
+        }
     }
 
     render() {
@@ -43,7 +80,7 @@ class UwaUpload extends React.Component {
         })
         return (
             <div>
-                <input type="file" ref="fileUploader" multiple={false} accept="images/*" name="fileUpl" onChange={this.handleChangeInput} style={hideDisplay} />
+                <input type="file" ref="fileUploader" multiple={false} accept={this.acceptedFile()} name="fileUpl" onChange={this.handleChangeInput} style={hideDisplay} />
                 <Image cloudName={this.state.cloudName} publicId={this.state.publicId} width="300" crop="scale" >
                 </Image><br />
                 <button onClick={this.handleChangeImage}>Load other image</button>
@@ -51,6 +88,8 @@ class UwaUpload extends React.Component {
                 <p>Example Upload then handle file</p>
                 <button onClick={this.handleClickUpload}>CLICK ME</button>
                 {fileList}
+                <br />
+                <button onClick={this.handleBeginUpload} style={this.renderDisplay()}>BEGIN UPLOAD</button>
             </div>
         );
     }
