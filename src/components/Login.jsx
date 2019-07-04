88d5e9913cfcbaf050b02c9ab6af471111bb1c44
anditakaesar <anditakaesar@gmail.com>
2019-07-04 16:18:30 +0700

    change class into className

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -5,15 +5,15 @@ class Login extends React.Component{
     render(){
       return(
         <div className="div-signin">
-          <form class="form-signin shadow text-center">
-          <h1 class=" text-center h1-login">LOGIN</h1>
-          <img src={logo} width="150" class="text-center"></img>
-          <label htmlFor="inputUsername" class="sr-only">Username</label>
+          <form className="form-signin shadow text-center">
+          <h1 className=" text-center h1-login">LOGIN</h1>
+          <img src={logo} width="150" className="text-center"></img>
+          <label htmlFor="inputUsername" className="sr-only">Username</label>
           <input type="text" className="form-control" placeholder="Username"></input>
-          <label htmlFor="inputPassword" class="sr-only">Password</label>
+          <label htmlFor="inputPassword" className="sr-only">Password</label>
           <input type="password" className="form-control" placeholder="Password"></input>
           <button type="submit" className="btn btn-lg btn-block btn-big-pink">Log In</button>
-          <p class="mt-5 mb-3 text-muted">&copy; 2019 </p>
+          <p className="mt-5 mb-3 text-muted">&copy; 2019 </p>
         </form>
         </div> 
       );
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,28 +5,28 @@ class Main extends React.Component{
       return(
         <div>
           <h1>Welcome,</h1>
-          <div class="btn-group">
-            <button type="button" class="btn btn-big-pink dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <div className="btn-group">
+            <button type="button" className="btn btn-big-pink dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               User
             </button>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Separated link</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Separated link</a>
             </div>
           </div>
-          <div class="btn-group">
-            <button type="button" class="btn btn-big-pink dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <div className="btn-group">
+            <button type="button" className="btn btn-big-pink dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Customer
             </button>
-            <div class="dropdown-menu">
-              <a class="dropdown-item" href="#">Action</a>
-              <a class="dropdown-item" href="#">Another action</a>
-              <a class="dropdown-item" href="#">Something else here</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Separated link</a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">Separated link</a>
             </div>
           </div>
         </div>
