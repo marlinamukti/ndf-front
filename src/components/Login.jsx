f606e1a1e47e810bfce8a5ebe64f09980d46eead
anditakaesar <anditakaesar@gmail.com>
2019-07-04 17:48:36 +0700

    implement state login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,54 @@
 import React from 'react';
 import logo from '../img/logo.png';
 
-class Login extends React.Component{
-    render(){
-      return(
-        <div className="div-signin">
-          <form className="form-signin shadow text-center">
-          <h1 className=" text-center h1-login">LOGIN</h1>
-          <img src={logo} width="150" className="text-center" alt="logo image"></img>
-          <label htmlFor="inputUsername" className="sr-only">Username</label>
-          <input type="text" className="form-control" placeholder="Username"></input>
-          <label htmlFor="inputPassword" className="sr-only">Password</label>
-          <input type="password" className="form-control" placeholder="Password"></input>
-          <button type="submit" className="btn btn-lg btn-block btn-big-pink">Log In</button>
-          <p className="mt-5 mb-3 text-muted">&copy; 2019 </p>
-        </form>
-        </div> 
-      );
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
+
+    handleUsernameChange = (event) => {
+        this.setState({
+            username: event.target.value
+        });
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({
+            password: event.target.value
+        });
+    }
+
+    handleLoginButton = (event) => {
+        console.log(`logging in with '${this.state.username}' and '${this.state.password}'`);
+    }
+
+    render() {
+        return(
+            <div className="div-signin">
+                <div className="form-signin shadow text-center">
+                    <h1 className=" text-center h1-login">LOGIN</h1>
+                    <img src={logo} width="150" className="text-center" alt="logo image"></img>
+
+                    <label htmlFor="inputUsername" className="sr-only">Username</label>
+                    <input type="text" 
+                    className="form-control" 
+                    placeholder="username" value={this.state.username} onChange={this.handleUsernameChange}></input>
+                    
+                    <label htmlFor="inputPassword" className="sr-only">Password</label>
+                    <input type="password" 
+                    className="form-control" 
+                    placeholder="password" value={this.state.password} onChange={this.handlePasswordChange}></input>
+                    
+                    <button type="submit" className="btn btn-lg btn-block btn-big-pink" onClick={this.handleLoginButton}>Log In</button>
+                    
+                    <p className="mt-5 mb-3 text-muted">&copy; 2019 </p>
+                </div>
+            </div> 
+        );
     }
 }
 
