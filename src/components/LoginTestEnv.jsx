24e6963432f194cb128bb447c84d6be82067dbc3
anditakaesar <anditakaesar@gmail.com>
2019-07-05 17:19:41 +0700

    adding tester for login logout

---
+++ src/components/LoginTestEnv.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import Login from './Login';
+import { connect } from 'react-redux';
+import {
+    BrowserRouter as Router,
+    Route,
+    Redirect,
+    withRouter
+} from 'react-router-dom';
+import UserInfo from './UserInfo';
+
+class PublicPage extends React.Component {
+    render() {
+        return <h2>Public Page</h2>;
+    }
+}
+
+class ProtectedPage extends React.Component {
+    render() {
+        return (
+        <React.Fragment>
+            <h2>Protected Page</h2><br />
+            <UserInfo />
+        </React.Fragment>
+        );
+    }
+}
+
+class LoginTestEnv extends React.Component {
+    
+    isLoggedIn() { // function to chec user is authenticated
+        if (this.props.user.isAuthenticated) {
+          return <Route path='/' component={ProtectedPage} />
+        } else {
+          return <Redirect to='/login' />
+        }
+    }
+
+    render() {
+        return(
+            <Router>
+                {this.isLoggedIn()}
+                <Route path='/login' component={Login} />
+            </Router>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    };
+}
+
+export default connect(mapStateToProps)(LoginTestEnv);
