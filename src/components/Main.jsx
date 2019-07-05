18af61d2522161a9cda05b1e9741c9410e7c8b2b
Marlina Mukti <marlinamukti@gmail.com>
2019-07-05 10:21:41 +0700

    removing menubar position

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,33 @@
 import React from 'react'; // always import this
+import './Main.css';
 
 class Main extends React.Component{ // create component
     render(){ // at least provide render() function that return a html element
       return(
         <div>
           <h1>Welcome,</h1>
-          <div className="btn-group">
+          <div className="div-menubar">
             <button type="button" className="btn btn-big-pink dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               User
             </button>
             <div className="dropdown-menu">
-              <button className="dropdown-item">Action</button>
-              <button className="dropdown-item">Another action</button>
-              <button className="dropdown-item">Something else here</button>
-              <div className="dropdown-divider"></div>
-              <button className="dropdown-item">Separated link</button>
+              <button className="dropdown-item">Role</button>
+              <button className="dropdown-item">Employee</button>
             </div>
           </div>
-          <div className="btn-group">
+          <div className="div-menubar">
             <button type="button" className="btn btn-big-pink dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Customer
             </button>
             <div className="dropdown-menu">
-              <button className="dropdown-item">Action</button>
-              <button className="dropdown-item">Another action</button>
-              <button className="dropdown-item">Something else here</button>
-              <div className="dropdown-divider"></div>
-              <button className="dropdown-item">Separated link</button>
+              <button className="dropdown-item">Customer</button>
+              <button className="dropdown-item">Member</button>
             </div>
+          </div>
+          <div className="div-menubar">
+            <button type="button" className="btn btn-big-pink">
+              Store
+            </button>
           </div>
         </div>
       );
